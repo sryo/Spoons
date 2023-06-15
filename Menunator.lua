f607e54d72b1e2325f12857b5b7fb2d375333961
Mateo Yadarola <teodalton@gmail.com>
2023-06-14 21:11:20 -0300

    Update Menunator.lua
    Supports detecting the dock in any position.

--- Menunator.lua
+++ Menunator.lua
@@ -5,16 +5,36 @@ local killDock = true           -- prevent the dock from appearing
 local onlyFullscreen = true     -- but only on fullscreen spaces
 local buffer = 4                -- increase if you still manage to activate them
 
+function getDockPosition()
+    local handle = io.popen("defaults read com.apple.dock orientation")
+    local result = handle:read("*a")
+    handle:close()
+    return result:gsub("^%s*(.-)%s*$", "%1")
+end
+
+local dockPos = getDockPosition()
+
 ev_tap = hs.eventtap.new({hs.eventtap.event.types.mouseMoved}, function(e)
     local win = hs.window.focusedWindow()
-    local screenHeight = win:screen():fullFrame().h
+    local screenFrame = win:screen():fullFrame()
+    local screenHeight = screenFrame.h
+    local screenWidth = screenFrame.w
+
     if onlyFullscreen and win and win:isFullScreen() then
         if killMenu and e:location().y < buffer then
             hs.console.printStyledtext("🔪🍔 Menu bar killed")
             return true
-        elseif killDock and (screenHeight - e:location().y) < buffer then
-            hs.console.printStyledtext("🔪🚢 Dock killed")
-            return true
+        elseif killDock then
+            if dockPos == "bottom" and (screenHeight - e:location().y) < buffer then
+                hs.console.printStyledtext("🔪🚢 Dock killed")
+                return true
+            elseif dockPos == "left" and e:location().x < buffer then
+                hs.console.printStyledtext("🔪🚢 Dock killed")
+                return true
+            elseif dockPos == "right" and (screenWidth - e:location().x) < buffer then
+                hs.console.printStyledtext("🔪🚢 Dock killed")
+                return true
+            end
         end
     end
     return false
