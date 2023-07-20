9d1a7d30ef78b1370701ea67d3e70c3077ae49b4
Mateo Yadarola <teodalton@gmail.com>
2023-07-20 18:14:26 -0300

    Update TrackpadWizard.lua
    Gesture Craft Mode.

--- TrackpadWizard.lua
+++ TrackpadWizard.lua
@@ -1,3 +1,6 @@
+-- TrackpadWizard: https://github.com/sryo/Spoons/blob/main/TrackpadWizard.lua
+-- This enables customizable trackpad gestures, allowing users to define zones on their trackpad that can trigger specific actions, and includes a gesture crafting mode for defining new zones.
+
 local config = {
     forceThreshold         = 20,
     scrollUnit             = "pixel",
@@ -10,6 +13,8 @@ local activeTouches = {}
 local lastScrollTime = nil
 local scrollDirection = -1
 local gestureEnded = false
+local gestureCraft = false
+local craftTouchStart, craftTouchEnd = nil, nil
 
 local function keyRepeatAction(touchID, modifiers, key)
     activeTouches[touchID] = hs.timer.doWhile(
@@ -57,7 +62,53 @@ local zones = {
             end,
         }
     },
+-- To add a new zone, define the friendlyName, 
+-- x and y coordinates, width and height, and the actions for the zone.
+-- The x and y coordinates, as well as width and height are normalized 
+-- (0 - 1) relative to the trackpad's surface.
+-- 'friendlyName' is a name for the zone that will be printed when the zone is touched.
+-- 'actions' is a table that maps modifier key states to functions that are 
+-- called when the zone is touched with that modifier state.
 }
+
+local zoneCounter = 0
+
+local function enterGestureCraft()
+    gestureCraft = true
+    print("Gesture Craft mode entered. Please make a diagonal gesture across your desired zone.")
+    zoneCounter = zoneCounter + 1
+end
+
+local function exitGestureCraft()
+    if craftTouchStart and craftTouchEnd then
+        local zoneCoordinates = {
+            x = math.min(craftTouchStart.x, craftTouchEnd.x),
+            y = math.min(craftTouchStart.y, craftTouchEnd.y),
+            width = math.abs(craftTouchEnd.x - craftTouchStart.x),
+            height = math.abs(craftTouchEnd.y - craftTouchStart.y)
+        }
+        print(string.format("New Zone coordinates: x = %.2f, y = %.2f, width = %.2f, height = %.2f", 
+            zoneCoordinates.x, zoneCoordinates.y, zoneCoordinates.width, zoneCoordinates.height))
+        local newZoneName = "Zone" .. zoneCounter
+        table.insert(zones, {
+            friendlyName = newZoneName,
+            x = zoneCoordinates.x,
+            y = zoneCoordinates.y,
+            width = zoneCoordinates.width,
+            height = zoneCoordinates.height,
+            actions = {
+                ["none"] = function(touchID)
+                    print(string.format("Touching %s with coordinates: x = %.2f, y = %.2f, width = %.2f, height = %.2f",
+                                        newZoneName, zoneCoordinates.x, zoneCoordinates.y, zoneCoordinates.width, zoneCoordinates.height))
+                end
+            }
+        })
+        craftTouchStart, craftTouchEnd = nil, nil
+    else
+        print("No valid diagonal gesture detected.")
+    end
+    gestureCraft = false
+end
 
 local function resetScrollDirection()
     scrollDirection = -1
@@ -75,43 +126,51 @@ end
 eventtap = hs.eventtap.new({hs.eventtap.event.types.gesture}, function(e)
     local touches = e:getTouches()
     for i, touch in ipairs(touches) do
-        if touch.normalizedPosition and touch.touching and touch.force >= config.forceThreshold then
-            local activeZone = getActiveZone(touch.normalizedPosition)
-            if activeZone then
-                local modifiers = hs.eventtap.checkKeyboardModifiers()
-                local modifierState = "none"
+        if gestureCraft then
+            if touch.touching then
+                if not craftTouchStart then
+                    craftTouchStart = touch.normalizedPosition
+                end
+                craftTouchEnd = touch.normalizedPosition
+            elseif not touch.touching and craftTouchStart and craftTouchEnd then
+                exitGestureCraft()
+            end
+        else
+            if touch.normalizedPosition and touch.touching and touch.force >= config.forceThreshold then
+                local activeZone = getActiveZone(touch.normalizedPosition)
+                if activeZone then
+                    local modifiers = hs.eventtap.checkKeyboardModifiers()
+                    local modifierState = "none"
 
-                local modifierKeysOrder = {"cmd", "alt", "shift", "ctrl", "fn"}
+                    local modifierKeysOrder = {"cmd", "alt", "shift", "ctrl", "fn"}
 
-                for _, key in ipairs(modifierKeysOrder) do
-                    if modifiers[key] then
-                        modifierState = (modifierState == "none") and key or (modifierState .. "+" .. key)
+                    for _, key in ipairs(modifierKeysOrder) do
+                        if modifiers[key] then
+                            modifierState = (modifierState == "none") and key or (modifierState .. "+" .. key)
+                        end
                     end
-                end
 
-                local action = activeZone.actions[modifierState]
-                if action and not activeTouches[touch.identity] then
-                    action(touch.identity)
+                    local action = activeZone.actions[modifierState]
+                    if action and not activeTouches[touch.identity] then
+                        action(touch.identity)
+                    end
+                    gestureEnded = false
                 end
-                gestureEnded = false
+            elseif not touch.touching and activeTouches[touch.identity] then
+                activeTouches[touch.identity]:stop()
+                activeTouches[touch.identity] = nil
+                if getActiveZone(touch.normalizedPosition) and getActiveZone(touch.normalizedPosition).friendlyName == "Scroll" then
+                    scrollDirection = -scrollDirection
+                end
+                lastScrollTime = os.time() 
+                gestureEnded = true
             end
-        elseif not touch.touching and activeTouches[touch.identity] then
-            activeTouches[touch.identity]:stop()
-            activeTouches[touch.identity] = nil
-            if getActiveZone(touch.normalizedPosition) and getActiveZone(touch.normalizedPosition).friendlyName == "Scroll" then
-                scrollDirection = -scrollDirection
-            end
-            lastScrollTime = os.time() 
-            gestureEnded = true
-        end
-    end
 
-    -- Added condition for touches outside active zones
-    for i, touch in ipairs(touches) do
-        if touch.normalizedPosition and not getActiveZone(touch.normalizedPosition) and activeTouches[touch.identity] then
-            activeTouches[touch.identity]:stop()
-            activeTouches[touch.identity] = nil
-            gestureEnded = true
+            if touch.normalizedPosition and not getActiveZone(touch.normalizedPosition) and activeTouches[touch.identity] then
+                activeTouches[touch.identity]:stop()
+                activeTouches[touch.identity] = nil
+                gestureEnded = true
+            end
         end
     end
 
@@ -119,8 +178,6 @@ eventtap = hs.eventtap.new({hs.eventtap.event.types.gesture}, function(e)
         resetScrollDirection()
         lastScrollTime = nil
     end
-
-    return false
 end)
 
 eventtap:start()
@@ -128,3 +185,5 @@ eventtap:start()
 hs.window.filter.new():subscribe({ hs.window.filter.windowFocused }, function()
     resetScrollDirection()
 end)
+
+hs.hotkey.bind({"ctrl", "shift"}, "G", enterGestureCraft)
