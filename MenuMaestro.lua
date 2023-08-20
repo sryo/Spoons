33beff6dcf20ed7a580aa091f948c452a3f01c73
Mateo Yadarola <teodalton@gmail.com>
2023-08-20 04:51:06 +0000

    Update MenuMaestro.lua
    Add a 4 finger gesture to show the chooser.

--- MenuMaestro.lua
+++ MenuMaestro.lua
@@ -6,15 +6,15 @@ local chooser = require("hs.chooser")
 local styledtext = require("hs.styledtext")
 local canvas = require("hs.canvas")
 
-local sortAlphabetically = true  -- Set to false for hierarchical sorting.
-local maxRecentItems = 5         -- You can change this to store a different number of recent items
+local sortAlphabetically = true -- Set to false for hierarchical sorting.
+local maxRecentItems = 5        -- You can change this to store a different number of recent items
 
 local recentItems = {}
 
 local function shortcutToString(modifiers, shortcut)
     local str = ""
     local keyMap = {
-        cmd = "⌘", ctrl = "⌃", alt = "⌥",  shift = "⇧", ["\xEF\x9C\x84"] = "F1", ["\xEF\x9C\x85"] = "F2", ["\xEF\x9C\x86"] = "F3", ["\xEF\x9C\x87"] = "F4", ["\xEF\x9C\x88"] = "F5", ["\xEF\x9C\x89"] = "F6", ["\xEF\x9C\x8A"] = "F7", ["\xEF\x9C\x8B"] = "F8", ["\xEF\x9C\x8C"] = "F9", ["\xEF\x9C\x8D"] = "F10", ["\xEF\x9C\x8E"] = "F11", ["\xEF\x9C\x8F"] = "F12", ["\xEF\x9C\x80"] = "▶", ["\xEF\x9C\x81"] = "◀", ["\xEF\x9C\x82"] = "▲", ["\xEF\x9C\x83"] = "▼", ["\x1B"] = "⎋", ["\x0D"] = "↩", ["\x08"] = "⌫", ["\x7F"] = "⌦", ["\x09"] = "⇥", ["\xE2\x84\xAA"] = "⇪", ["\xE2\x87\x9E"] = "⇞", ["\xE2\x87\x9F"] = "⇟", ["\xE2\x86\x96"] = "↖", ["\xE2\x86\x98"] = "↘"
+        cmd = "⌘", ctrl = "⌃", alt = "⌥", shift = "⇧", ["\xEF\x9C\x84"] = "F1", ["\xEF\x9C\x85"] = "F2", ["\xEF\x9C\x86"] = "F3", ["\xEF\x9C\x87"] = "F4", ["\xEF\x9C\x88"] = "F5", ["\xEF\x9C\x89"] = "F6", ["\xEF\x9C\x8A"] = "F7", ["\xEF\x9C\x8B"] = "F8", ["\xEF\x9C\x8C"] = "F9", ["\xEF\x9C\x8D"] = "F10", ["\xEF\x9C\x8E"] = "F11", ["\xEF\x9C\x8F"] = "F12", ["\xEF\x9C\x80"] = "▶", ["\xEF\x9C\x81"] = "◀", ["\xEF\x9C\x82"] = "▲", ["\xEF\x9C\x83"] = "▼", ["\x1B"] = "⎋", ["\x0D"] = "↩", ["\x08"] = "⌫", ["\x7F"] = "⌦", ["\x09"] = "⇥", ["\xE2\x84\xAA"] = "⇪", ["\xE2\x87\x9E"] = "⇞", ["\xE2\x87\x9F"] = "⇟", ["\xE2\x86\x96"] = "↖", ["\xE2\x86\x98"] = "↘"
     }
 
     if shortcut and shortcut ~= "" then
@@ -30,7 +30,7 @@ end
 
 local function shortcutToImage(modifiers, shortcut)
     local text = shortcutToString(modifiers, shortcut or "")
-    local textCanvas = canvas.new{ x = 0, y = 0, w = 32, h = 32 }
+    local textCanvas = canvas.new { x = 0, y = 0, w = 32, h = 32 }
 
     local textColor
     if hs.host.interfaceStyle() == "Dark" then
@@ -64,7 +64,7 @@ local function collectMenuItems(menuPath, path, list, choices)
         for i, title in ipairs(path) do
             pathList[i] = title
         end
-        pathList[#pathList+1] = title
+        pathList[#pathList + 1] = title
         if item.AXChildren then
             collectMenuItems(currentMenuPath, pathList, item.AXChildren[1], choices)
         elseif item.AXEnabled and title and (not (title == '')) then
@@ -76,7 +76,7 @@ local function collectMenuItems(menuPath, path, list, choices)
             end
             local shortcut = item.AXMenuItemCmdChar or ""
             local shortcutImage = shortcutToImage(modifiers, shortcut or "")
-            choices[#choices+1] = {
+            choices[#choices + 1] = {
                 text = styledtext.new(title),
                 plainText = title,
                 subText = currentMenuPath,
@@ -87,7 +87,7 @@ local function collectMenuItems(menuPath, path, list, choices)
     end
 end
 
-local function chooseMenuItem()
+function openMenuMaestro()
     local app = hs.application.frontmostApplication()
     local appName = app:name()
 
@@ -139,12 +139,29 @@ local function chooseMenuItem()
             end
         end
 
-        local menuItemChooser = chooser.new(completionFn)
-        menuItemChooser:choices(choices)
-        menuItemChooser:searchSubText(true)
-        menuItemChooser:placeholderText("Find a menu item in " .. appName .. "...")
-        menuItemChooser:show()
+        menuMaestro = chooser.new(completionFn)
+        menuMaestro:choices(choices)
+        menuMaestro:searchSubText(true)
+        menuMaestro:placeholderText("Find a menu item in " .. appName .. "...")
+        menuMaestro:show()
     end)
 end
 
-hs.hotkey.bind({"ctrl", "alt"}, "space", chooseMenuItem)
+function isMenuMaestroOpen()
+    return menuMaestro and menuMaestro:isVisible()
+end
+
+function touchToOpenMenuMaestro(event)
+    local touches = event:getTouches()
+    if touches and #touches == 4 and not wasHandled and not isMenuMaestroOpen() then
+        wasHandled = true
+        openMenuMaestro()
+    elseif touches and #touches < 4 then
+        wasHandled = false
+    end
+end
+
+gestureTap = hs.eventtap.new({ hs.eventtap.event.types.gesture }, touchToOpenMenuMaestro)
+gestureTap:start()
+
+hs.hotkey.bind({ "ctrl", "alt" }, "space", openMenuMaestro)
