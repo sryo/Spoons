6e9f41a8ee79f5b0b5a473c94ff1523361c6fbe8
Mateo Yadarola <teodalton@gmail.com>
2025-01-15 17:51:10 -0300

    Create AppTimeout.lua

---
+++ AppTimeout.lua
@@ -0,0 +1,37 @@
+-- AppTimeout: automatically close apps that have no windows
+
+local windowlessApps = {}
+
+local function checkApps()
+    for _, app in ipairs(hs.application.runningApplications()) do
+        local name = app:name()
+        if app:kind() == 1 and
+            name ~= "Finder" and
+            name ~= "Dock" and
+            name ~= "SystemUIServer" and
+            name ~= "ControlCenter" and
+            name ~= "Transmission" and
+            name ~= "Calendar" and
+            name ~= "Stickies" and
+            name ~= "WhatsApp" and
+            name ~= "Activity Monitor" and
+            name ~= "Hammerspoon" then
+            if #app:allWindows() == 0 then
+                if not windowlessApps[name] then
+                    windowlessApps[name] = os.time()
+                    print(string.format("Monitoring %s", name))
+                elseif os.time() - windowlessApps[name] >= 300 then
+                    print(string.format("Closing %s", name))
+                    app:kill()
+                    windowlessApps[name] = nil
+                end
+            elseif windowlessApps[name] then
+                windowlessApps[name] = nil
+                print(string.format("Stopped monitoring %s", name))
+            end
+        end
+    end
+end
+
+checkAppsTimer = hs.timer.new(60, checkApps):start()
+print("AppTimeout is running")
